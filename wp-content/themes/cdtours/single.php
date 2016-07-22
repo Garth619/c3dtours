1ec761457c6b619881081ab8dab4db0ae44aa65d
garrett cullen <info@garrettcullen.com>
2016-07-21 20:23:13 -0700

    push

--- wp-content/themes/cdtours/single.php
+++ wp-content/themes/cdtours/single.php
@@ -1,28 +1,8 @@
-<?php
-/**
- * Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-get_header(); ?>
+				
+						<?php the_content(); ?>
+						
+					
 
-		<div id="container">
-			<div id="content" role="main">
-
-			<?php
-			/*
-			 * Run the loop to output the post.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-single.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'single' );
-			?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php endwhile; // end of the loop. ?>
