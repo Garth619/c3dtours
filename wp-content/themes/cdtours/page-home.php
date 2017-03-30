26e09c5accc2a935f79254406d78ea946bc7a876
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-03-29 19:49:20 -0700

    Push

--- wp-content/themes/cdtours/page-home.php
+++ wp-content/themes/cdtours/page-home.php
@@ -32,17 +32,9 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/slick/slick/slick.css"/>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/slick/slick/slick-theme.css"/>
 <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/wow/css/libs/animate.css">
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory');?>/fonts/stylesheet.css" />
 <?php wp_head();?>
-<script>
-  (function(d) {
-    var config = {
-      kitId: 'teb2ngw',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
+
 </head>
 
 <body id="bodytop" <?php body_class(); ?>>
